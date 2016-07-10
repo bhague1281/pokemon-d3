98f343d1ce3caa0873fc9bc25a17726a38e07fbf
Brian Hague <bhague1281@gmail.com>
2016-07-10 13:49:24 -0700

    added fetch, modularized node functionality

--- js/app.js
+++ js/app.js
@@ -6,42 +6,46 @@ var nodeWidth = 10;
 var pokeGraph = d3.select('#pokemon')
                   .append('svg')
                   .attr('height', height)
-                  .attr('width', width)
+                  .attr('width', width);
 
 function randomColor() {
   var red = Math.floor(Math.random() * 255);
   var green = Math.floor(Math.random() * 255);
   var blue = Math.floor(Math.random() * 255);
-  return 'rgb(' + red + ',' + green + ',' + blue + ')'
+  return 'rgb(' + red + ',' + green + ',' + blue + ')';
 }
 
-fetch('http://pokeapi.co/api/v2/pokemon/?limit=' + pokemonLimit, { cache: 'default'})
+function drawPokemonNodes(json) {
+  var pokemon = json.results.map(function(monster) {
+    monster.x = Math.random() * (width - nodeWidth * 2) + nodeWidth;
+    monster.y = Math.random() * (height - nodeWidth * 2) + nodeWidth;
+    return monster;
+  });
+
+  // add node containers
+  var nodes = pokeGraph.selectAll('.node')
+    .data(pokemon)
+    .enter().append('g');
+
+  // add circles
+  nodes.append('svg:circle')
+    .attr('class', 'node')
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; })
+    .attr('r', nodeWidth)
+    .attr('fill', function() { return randomColor(); });
+
+  // add text
+  nodes.append('text')
+    .attr('dx', function(d) { return d.x - nodeWidth; })
+    .attr('dy', function(d) { return d.y - nodeWidth; })
+    .text(function(d) { return d.name; });
+}
+
+fetch('http://pokeapi.co/api/v2/pokemon/?limit=' + pokemonLimit, { cache: 'default' })
   .then(function(response) {
     return response.json();
   })
   .then(function(json) {
-    var pokemon = json.results.map(function(pokemon) {
-      pokemon.x = Math.random() * (width - nodeWidth * 2) + nodeWidth;
-      pokemon.y = Math.random() * (height - nodeWidth * 2) + nodeWidth;
-      return pokemon;
-    });
-
-    // add node containers
-    var node = pokeGraph.selectAll('.node')
-      .data(pokemon)
-      .enter().append('g');
-
-    // add circles
-    node.append('svg:circle')
-      .attr('class', 'node')
-      .attr('cx', function(d) { return d.x; })
-      .attr('cy', function(d) { return d.y; })
-      .attr('r', nodeWidth)
-      .attr('fill', function() { return randomColor(); });
-
-    // add text
-    node.append('text')
-      .attr('dx', function(d) { return d.x - nodeWidth; })
-      .attr('dy', function(d) { return d.y - nodeWidth; })
-      .text(function(d) { return d.name; })
+    drawPokemonNodes(json);
   });
